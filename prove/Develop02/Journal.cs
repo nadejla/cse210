789321618da1a7002e963656d8d446b5b8f5e012
nadejla <nadinelugo@gmail.com>
2024-03-07 20:35:22 -0700

    Stubbing out Journal Project.

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,23 @@
+public class Journal
+{
+    public List<Entry> _entries;
+    // public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry()
+    {
+        // Console.WriteLine("This function will enable the user to add an entry.");
+    }
+    public void DisplayAll()
+    {
+        // Console.WriteLine("This function will display all the entries.");
+    }
+    public void SaveToFile(string file)
+    {
+        // Console.WriteLine("This function will save the entry to a file.");
+    }
+    public void LoadFromFile(string file)
+    {
+        // Console.WriteLine("This function will load the entries from a file.");
+    }
+
+}
---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,11 @@
+public class PromptGenerator
+{
+    public List<string> _prompts;
+    // public List<string> _prompts = new List<string>();
+
+    public string GetRandomPrompt()
+    {
+        string prompt = "This function will return a prompt.";
+        return prompt;
+    }
+}
